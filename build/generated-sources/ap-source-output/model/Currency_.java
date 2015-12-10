5f28b2f5be7d9c60d52a7f2f0150338ac352ebf1
sthlmj <joehuldens@gmail.com>
2015-12-10 14:07:00 +0100

    ricchhhhh

--- build/generated-sources/ap-source-output/model/Currency_.java
+++ build/generated-sources/ap-source-output/model/Currency_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-10T13:46:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-10T14:04:09")
 @StaticMetamodel(Currency.class)
 public class Currency_ { 
 
